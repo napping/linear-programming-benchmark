37406095e2e431fc76a92942e053adcbfcb17c9d
Brian Shi <brishi@seas.upenn.edu>
2016-05-13 10:36:48 -0400

    Build tools.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LinearProgrammingBenchmark',
+    description="This is a simulation of the greedy and linear programming algorithm for the minimum element game.",
+    version='0.0.1',
+    packages=find_packages(),
+    author='Brian Shi, David Xu',
+    author_email='brishi@seas.upenn.edu, davix@seas.upenn.edu',
+    long_description=open('README.md').read(),
+)
